2aec0377bb724c3b5080dc9bdd4405e82abb9577
unknown <Administrador@ALGUARISA.LOCAL>
2026-03-06 16:04:48 -0400

    filament

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,7 +11,9 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+        $middleware->web(append: [
+            \Illuminate\Http\Middleware\SetCacheHeaders::class . ':no_store;max_age=0',
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,14 @@
 
 use Illuminate\Support\Facades\Route;
 
+Route::get('login', function () {
+    return view('pages.auth.login');
+})->middleware('guest')->name('login');
+
 Route::view('/', 'welcome')->name('home');
 
-Route::middleware(['auth', 'verified'])->group(function () {
+/*Route::middleware(['auth', 'verified'])->group(function () {
     Route::view('dashboard', 'dashboard')->name('dashboard');
-});
+});*/
 
 require __DIR__.'/settings.php';
